b1710a359c3b77fac4ce9f371b3135ba5108a21a
링크상태 <60739875+Link-State@users.noreply.github.com>
2024-01-25 17:28:30 +0900

    init

---
+++ TowerDefense/src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+		Bukkit.getPluginManager();
+	}
+	
+	/*
+	 * /protectTower
+	 * /protectTower start <name> <username...>
+	 * 
+	 */
+}
